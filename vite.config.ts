a2d8fdb676b6fa40b92087b2baaae0f955c76efd
Tygebot <124087313+Tygebot@users.noreply.github.com>
2025-04-23 00:17:26 -0400

    root?

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Ensure the base URL is set to the root
   plugins: [vue()],
   css: {
     preprocessorOptions: {
